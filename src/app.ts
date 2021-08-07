b36c025f838d857df937fa0c30d4210216e9800f
Arun Raj R <arunraj6@hotmail.com>
2021-08-07 19:46:37 +0530

    add business service to process the data

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 const fs = require('fs');
 const StreamArray = require('stream-json/streamers/StreamArray'); // To process huge JSON files while requiring a minimal memory
+const BMIService = require('./bmi/bmi.service.ts'); // Business module
 
 const jsonStream = StreamArray.withParser();
-fs.createReadStream('src/input/person.json').pipe(jsonStream.input); // To read the input JSON data
+fs.createReadStream('src/bmi/person.json').pipe(jsonStream.input); // To read the input JSON data
 
+const bmiService = new BMIService();
 jsonStream.on('data', ({ key, value }) => {
-    console.log(key, value);
+    bmiService.calculateBMI(key, value);
 });
 
 jsonStream.on('end', () => {
-    console.log('All Done');
+    console.log('Finished!');
 });
---
+++ src/bmi/bmi.service.ts
@@ -0,0 +1,26 @@
+module.exports = class BMIService {
+    //#region PUBLIC METHODS
+
+    /**
+    * Calculate BMI per Person and print the result
+    * 
+    * @param key is the array-index
+    * @param value Each Person record from the input
+    * @returns void, Empty result
+    * @returns Exception, @see [Base Exceptions](https://docs.nestjs.com/exception-filters#base-exceptions)
+    */
+    calculateBMI(key, value) {
+        try {
+            console.log(key, value);
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+
+    //#endregion
+
+    //#region PRIVATE METHODS
+
+    //#endregion
+}
